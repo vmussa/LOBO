be8fa5a92ae0feb768f996614f1f27b5cfb88f1e
Viclib <vh@viclib.com>
2013-05-08 07:56:23 -0300

    Protótipo inicial do jogo: personagem-palito que gira, pula e se pendura em blocos.

--- lobo.js
+++ lobo.js
@@ -4,122 +4,183 @@ if (typeof window == 'undefined' || window === null) {
   prelude.installPrelude(window);
 }
 (function(){
-  var viclib, box2d, $, THREE, v3, spr, bind, vec, person, last_tick, tick, particle, proc;
+  var viclib, box2d, $, THREE, v3, key, cycle, interpolate, now, arms, Monkey, monkey, last_tick, proc;
   viclib = require('viclib')();
   box2d = require('box2d');
   $ = require('boxes');
   THREE = require('three');
   v3 = require('victhree').v3;
-  window.vectors = [];
-  spr = {
-    bre: 0.2,
-    hea: 0,
-    el1: 0.8,
-    el2: 0.86,
-    hn1: 0.75,
-    hn2: 0.75,
-    kn1: 0.8,
-    kn2: 0.86,
-    ft1: 0.75,
-    ft2: 0.75
-  };
-  bind = function(a, b){
-    a.bounds.push({
-      vector: b,
-      pos: b.pos.clone().sub(a.pos)
-    });
-    return b.bounds.push({
-      vector: a,
-      pos: a.pos.clone().sub(b.pos)
-    });
-  };
-  vec = function(x, y){
-    var vec;
-    vec = {
-      pos: v3(x, y, 0),
-      vel: v3(0, 0, 0),
-      bounds: [],
-      locked: false
-    };
-    vectors.push(vec);
-    return vec;
-  };
-  person = function(){
-    var head, breast, lelbow, relbow, lhand, rhand, pelvis, lknee, rknee, lfoot, rfoot, i$, ref$, len$, v;
-    head = vec(0, -25);
-    breast = vec(0, -15);
-    lelbow = vec(-5, -5);
-    relbow = vec(5, -5);
-    lhand = vec(-5, 5);
-    rhand = vec(5, 5);
-    pelvis = vec(0, 0);
-    lknee = vec(-5, 15);
-    rknee = vec(5, 15);
-    lfoot = vec(-5, 30);
-    rfoot = vec(5, 30);
-    for (i$ = 0, len$ = (ref$ = [head, breast, lelbow, relbow, lhand, rhand, pelvis, lknee, rknee, lfoot, rfoot]).length; i$ < len$; ++i$) {
-      v = ref$[i$];
-      v.pos.add(v3(200, 200, 0));
-    }
-    bind(head, breast);
-    bind(breast, lelbow);
-    bind(breast, relbow);
-    bind(breast, pelvis);
-    bind(lelbow, lhand);
-    bind(relbow, rhand);
-    bind(pelvis, rknee);
-    bind(pelvis, lknee);
-    bind(lknee, lfoot);
-    return bind(rknee, rfoot);
-  };
-  last_tick = Date.now();
-  tick = function(){
-    var dt, i$, ref$, len$, a, j$, ref1$, len1$, bound, b, b_to_a, stable_b_pos;
-    dt = (Date.now() - last_tick) / 1000;
-    last_tick = Date.now();
-    for (i$ = 0, len$ = (ref$ = vectors).length; i$ < len$; ++i$) {
-      a = ref$[i$];
-      a.pos.add(a.vel.clone().multiplyScalar(dt));
-      a.pos.z += 1;
-      for (j$ = 0, len1$ = (ref1$ = a.bounds).length; j$ < len1$; ++j$) {
-        bound = ref1$[j$];
-        b = bound.vector;
-        b_to_a = a.pos.clone().sub(b.pos).normalize();
-        stable_b_pos = a.pos.clone().add(b_to_a.multiplyScalar(-bound.dist));
-        b.pos = stable_b_pos;
+  key = require('key');
+  cycle = function(period, stances){
+    return function(t){
+      var member, ref$, frames, results$ = {};
+      for (member in ref$ = stances) {
+        frames = ref$[member];
+        results$[member] = interpolate(frames, t / period);
       }
-      a.vel.multiplyScalar(Math.pow(0.7, dt));
-      vectors[0].pos.add(v3(mousex, mousey, 0).clone().sub(vectors[0].pos).normalize().multiplyScalar(5 * dt));
-      vectors[0].vel = v3(0, 0, 0);
-    }
-    return setTimeout(tick, 15);
+      return results$;
+    };
   };
-  particle = mixin(function(){
+  interpolate = function(arr, n){
+    var part;
+    part = (n % 1) * (arr.length - 1);
+    return arr[floor(part)] * (1 - part % 1) + arr[floor(part + 1)] * (part % 1);
+  };
+  now = function(){
+    return Date.now() / 1000;
+  };
+  arms = [v3(100, 750, 0), v3(150, 720, 0), v3(200, 690, 0), v3(580, 700, 0), v3(630, 670, 0), v3(580, 640, 0), v3(630, 610, 0), v3(730, 200, 0), v3(580, 580, 0), v3(630, 550, 0), v3(580, 520, 0), v3(630, 490, 0), v3(580, 450, 0), v3(630, 420, 0), v3(580, 390, 0), v3(700, 230, 0), v3(200, 200, 0), v3(270, 310, 0), v3(220, 110, 0), v3(100, 50, 0), v3(330, 70, 0), v3(420, 370, 0), v3(320, 350, 0), v3(180, 290, 0), v3(220, 350, 0), v3(120, 360, 0), v3(160, 240, 0), v3(260, 240, 0), v3(460, 240, 0), v3(430, 190, 0), v3(400, 750, 0), v3(360, 770, 0)];
+  Monkey = mixin(function(){
+    var this$ = this;
     return {
-      pos: v3(0, 0, 0),
-      bounds: []
+      anatomy: {
+        bre: {
+          root: 'pel',
+          len: 0.17
+        },
+        hea: {
+          root: 'bre',
+          len: 0.06
+        },
+        el1: {
+          root: 'bre',
+          len: 0.08
+        },
+        hn1: {
+          root: 'el1',
+          len: 0.08
+        },
+        el2: {
+          root: 'bre',
+          len: 0.08
+        },
+        hn2: {
+          root: 'el2',
+          len: 0.08
+        },
+        kn1: {
+          root: 'pel',
+          len: 0.08
+        },
+        ft1: {
+          root: 'kn1',
+          len: 0.08
+        },
+        kn2: {
+          root: 'pel',
+          len: 0.08
+        },
+        ft2: {
+          root: 'kn2',
+          len: 0.08
+        }
+      },
+      climb: cycle(3, {
+        bre: [0.3, 0.4, 0.7, 0.9, 1.4, 1.8, 1.8, 1.6, 1.2, 0.8, 0.3],
+        hea: [0.4, 0.4, 0.7, 1.0, 1.5, 1.9, 1.9, 1.7, 1.2, 0.8, 0.4],
+        el1: [0.0, 0.2, 0.6, 0.9, 1.0, 1.7, 1.0, 0.9, 0.9, 0.3, 0.0],
+        hn1: [0.5, 0.6, 0.8, 1.0, 1.1, 1.6, 1.4, 1.1, 1.0, 0.7, 0.5],
+        el2: [0.0, 0.2, 0.6, 0.9, 1.0, 1.8, 1.3, 1.2, 0.9, 0.4, 0.0],
+        hn2: [0.5, 0.6, 0.8, 1.0, 1.2, 1.6, 1.5, 1.2, 0.9, 0.7, 0.5],
+        kn1: [3.0, 2.8, 2.8, 2.7, 3.3, 5.0, 4.8, 4.5, 3.8, 3.4, 3.0],
+        ft1: [2.5, 2.3, 2.0, 2.2, 3.2, 4.5, 4.8, 4.4, 3.7, 3.2, 2.5],
+        kn2: [2.7, 2.7, 2.8, 2.8, 3.8, 4.5, 4.7, 4.5, 3.4, 3.0, 2.7],
+        ft2: [2.3, 2.5, 2.7, 2.7, 3.0, 3.7, 3.7, 3.8, 3.4, 3.0, 2.3]
+      }),
+      jump: cycle(1, {
+        bre: [1.2, 1.2],
+        hea: [1.2, 1.2],
+        el1: [0.3, 0.3],
+        hn1: [0.5, 0.5],
+        el2: [0.5, 0.5],
+        hn2: [0.7, 0.7],
+        kn1: [3.8, 3.8],
+        ft1: [3.8, 3.8],
+        kn2: [3.7, 3.7],
+        ft2: [2.7, 2.7]
+      }),
+      pos: v3(100, 700, 0),
+      vel: v3(0, 0, 0),
+      fixed: void 8,
+      tick: function(dt){
+        var cycle, member, ref$, ang, ds;
+        this$.pts = {
+          pel: {
+            root: void 8,
+            pos: v3(0, 0, 0)
+          }
+        };
+        cycle = this$.fixed != null
+          ? this$.climb
+          : this$.jump;
+        for (member in ref$ = cycle(now())) {
+          ang = ref$[member];
+          this$.pts[member] = {
+            pos: v3(cos(ang * tau / 4), -sin(ang * tau / 4), 0).multiplyScalar(this$.anatomy[member].len * 150).add(this$.pts[this$.anatomy[member].root].pos),
+            root: this$.pts[this$.anatomy[member].root]
+          };
+        }
+        if (this$.fixed) {
+          ds = this$.fixed.pos.clone().sub(this$.pts[this$.fixed.pivot].pos);
+          each(function(it){
+            return it.pos.add(ds);
+          }, this$.pts);
+          this$.vel = this$.pts.pel.pos.clone().sub(this$.pos).multiplyScalar(1.65);
+          this$.pos = this$.pts.pel.pos;
+        } else {
+          this$.vel.add(v3(0, 3 * dt, 0));
+          this$.pos.add(this$.vel);
+          each(function(it){
+            return it.pos.add(this$.pos);
+          }, this$.pts);
+          each(function(it){
+            return this$.fixed = {
+              pos: it,
+              pivot: 'hn1'
+            };
+          })(
+          filter(function(it){
+            return it !== this$.last_arm || it === arms[0];
+          })(
+          filter(function(it){
+            return this$.pts.hn1.pos.distanceTo(it) < 22;
+          })(
+          arms)));
+        }
+        if (this$.pos.y > 900) {
+          this$.pos = v3(100, 700, 0);
+          return this$.vel = v3(0, 0, 0);
+        }
+      },
+      draw: function(proc){
+        proc.arc(this$.pts.hea.pos.x, this$.pts.hea.pos.y, 9, 9, 0, tau);
+        return each(function(it){
+          return proc.line(it.pos.x, it.pos.y, it.root.pos.x, it.root.pos.y);
+        })(
+        filter(function(it){
+          return it.root != null;
+        })(
+        this$.pts));
+      }
     };
   });
-  proc = processing(800, 600, function(){
-    var i$, ref$, len$, a, lresult$, j$, ref1$, len1$, bound, b, results$ = [];
+  monkey = Monkey();
+  key.press('space', function(){
+    return monkey.fixed = void 8;
+  });
+  last_tick = now();
+  proc = processing(800, 800, function(){
+    var dt, this$ = this;
+    dt = now() - last_tick;
+    last_tick = now();
     this.background(240, 240, 240);
     this.strokeWeight(3);
-    for (i$ = 0, len$ = (ref$ = vectors).length; i$ < len$; ++i$) {
-      a = ref$[i$];
-      lresult$ = [];
-      for (j$ = 0, len1$ = (ref1$ = a.bounds).length; j$ < len1$; ++j$) {
-        bound = ref1$[j$];
-        b = bound.vector;
-        if (a.pos != null && b.pos != null) {
-          lresult$.push(this.line(a.pos.x, a.pos.y, b.pos.x, b.pos.y));
-        }
-      }
-      results$.push(lresult$);
-    }
-    return results$;
+    monkey.tick(dt);
+    monkey.draw(this);
+    return each(function(it){
+      return this$.rect(it.x, it.y, 5, 5);
+    }, arms);
   });
   proc.css('border', "1px solid black");
   $("body").append(proc);
-  person();
-  tick();
 }).call(this);
--- lobo.ls
+++ lobo.ls
@@ -1,127 +1,101 @@
-require! {viclib:viclib(), box2d:box2d, $:boxes, THREE:three, v3:victhree.v3}
-
-window.vectors = []
-
-spr =
-	bre: 0.2
-	hea: 0
-	el1: 0.8
-	el2: 0.86
-	hn1: 0.75
-	hn2: 0.75
-	kn1: 0.8
-	kn2: 0.86
-	ft1: 0.75
-	ft2: 0.75
-
-bind = (a,b) ->
-	a.bounds.push({vector:b, pos:b.pos.clone().sub(a.pos)})
-	b.bounds.push({vector:a, pos:a.pos.clone().sub(b.pos)})
-
-vec = (x,y) ->
-	vec = 
-		pos: v3(x,y,0)
-		vel: v3(0,0,0)
-		bounds: []
-		locked: false
-	vectors.push vec
-	vec
-
-person = -> 
-	head = vec(0,-25)
-
-	breast = vec(0,-15)
-	lelbow = vec(-5,-5)
-	relbow = vec(5,-5)
-
-	lhand = vec(-5,5)
-	rhand = vec(5,5)
-
-	pelvis = vec(0,0)
-
-	lknee = vec(-5,15)
-	rknee = vec(5,15)
-
-	lfoot = vec(-5,30)
-	rfoot = vec(5,30)
-
-	for v in [head, breast, lelbow, relbow, lhand, rhand, pelvis, lknee, rknee, lfoot, rfoot]
-		v.pos.add(v3(200,200,0))
-	bind(head,breast)
-	
-	bind(breast,lelbow)
-	bind(breast,relbow)
-	bind(breast,pelvis)
-
-	bind(lelbow,lhand)
-	bind(relbow,rhand)
-
-	bind(pelvis,rknee)
-	bind(pelvis,lknee)
-
-	bind(lknee,lfoot)
-	bind(rknee,rfoot)
-
-
-last_tick = Date.now!
-tick = ->
-	dt = (Date.now! - last_tick) / 1000
-	last_tick := Date.now!
-
-	for a in vectors
-		a.pos.add(a.vel.clone().multiplyScalar(dt))
-		a.pos.z += 1
-		#a.vel.add(v3(0,1,0))
-		#log a.pos
-		for bound in a.bounds
-			b = bound.vector
-			b_to_a = a.pos.clone().sub(b.pos).normalize()
-			stable_b_pos = a.pos.clone().add(b_to_a.multiplyScalar(-bound.dist))
-			#ds = stable_b_pos.clone().sub(b.pos)
-			#b.vel.add(ds)
-			b.pos = stable_b_pos
-
-
-			#if (b.pos.distanceTo(a.pos) < bound.dist) then
-				#b.vel.!k!
-
-			#force = ds.multiplyScalar(1)
-			#b.vel.add(force)
-			#log a.pos.clone().sub(b.pos).length() - bound.dist
-			#if Math.abs(a.pos.clone().sub(b.pos).length() - bound.dist) > 1 then
-				#b.vel = a.pos.clone().sub(b.pos).multiplyScalar(0.1)
-
-		a.vel.multiplyScalar(Math.pow(0.7,dt)) #kinetic energy loss
-
-		vectors[0].pos.add(v3(mousex,mousey,0).clone().sub(vectors[0].pos).normalize().multiplyScalar(5*dt))
-		vectors[0].vel = v3(0,0,0)
-		#vectors[0]
-			#..pos = v3(mousex,mousey,0)
-			#..vel = v3(0,0,0)
-
-	setTimeout tick, 15
-
-particle = mixin ->
-	pos: v3(0,0,0)
-	bounds: []
-
-proc = processing 800, 600, ->
+require! {viclib:viclib(), box2d:box2d, $:boxes, THREE:three, v3:victhree.v3,key}
+cycle = (period,stances) ->
+	(t) -> {[member,interpolate(frames,t/period)] for member,frames of stances}
+interpolate = (arr,n) ->
+	part = (n%1)*(arr.length-1)
+	arr[floor(part)]*(1 - part%1) + arr[floor(part+1)]*(part%1)
+now = -> Date.now!/1000
+arms = [v3(100,750,0),v3(150,720,0),v3(200,690,0),v3(580,700,0),v3(630,670,0),v3(580,640,0),v3(630,610,0),v3(730,200,0),
+	v3(580,580,0),v3(630,550,0),v3(580,520,0),v3(630,490,0),v3(580,450,0),v3(630,420,0),v3(580,390,0),v3(700,230,0),
+	v3(200,200,0),v3(270,310,0),v3(220,110,0),v3(100,50,0),v3(330,70,0),v3(420,370,0),v3(320,350,0),v3(180,290,0),
+	v3(220,350,0),v3(120,360,0),v3(160,240,0),v3(260,240,0),v3(460,240,0),v3(430,190,0),v3(400,750,0),v3(360,770,0)]
+Monkey = mixin ->
+	anatomy:
+		bre: root:\pel, len:0.17
+		hea: root:\bre, len:0.06
+		el1: root:\bre, len:0.08
+		hn1: root:\el1, len:0.08
+		el2: root:\bre, len:0.08
+		hn2: root:\el2, len:0.08
+		kn1: root:\pel, len:0.08
+		ft1: root:\kn1, len:0.08
+		kn2: root:\pel, len:0.08
+		ft2: root:\kn2, len:0.08
+	#walk: cycle 1,
+		#bre: [0.00 0.00 0.00 0.00 0.00]
+		#hea: [0.00 0.03 0.00 0.00 0.03]
+		#el1: [0.85 0.87 0.75 0.67 0.67]
+		#hn1: [0.85 0.72 0.72 0.67 0.63]
+		#el2: [0.67 0.67 0.79 0.85 0.85]
+		#hn2: [0.63 0.58 0.53 0.62 0.85]
+		#kn1: [0.85 0.87 0.75 0.67 0.67]
+		#ft1: [0.85 0.72 0.72 0.67 0.63]
+		#kn2: [0.67 0.67 0.79 0.85 0.85]
+		#ft2: [0.63 0.58 0.53 0.62 0.85]
+	climb: cycle 3,
+		bre: [0.3 0.4 0.7 0.9 1.4 1.8 1.8 1.6 1.2 0.8 0.3] 
+		hea: [0.4 0.4 0.7 1.0 1.5 1.9 1.9 1.7 1.2 0.8 0.4]
+		el1: [0.0 0.2 0.6 0.9 1.0 1.7 1.0 0.9 0.9 0.3 0.0]
+		hn1: [0.5 0.6 0.8 1.0 1.1 1.6 1.4 1.1 1.0 0.7 0.5]
+		el2: [0.0 0.2 0.6 0.9 1.0 1.8 1.3 1.2 0.9 0.4 0.0]
+		hn2: [0.5 0.6 0.8 1.0 1.2 1.6 1.5 1.2 0.9 0.7 0.5]
+		kn1: [3.0 2.8 2.8 2.7 3.3 5.0 4.8 4.5 3.8 3.4 3.0]
+		ft1: [2.5 2.3 2.0 2.2 3.2 4.5 4.8 4.4 3.7 3.2 2.5]
+		kn2: [2.7 2.7 2.8 2.8 3.8 4.5 4.7 4.5 3.4 3.0 2.7]
+		ft2: [2.3 2.5 2.7 2.7 3.0 3.7 3.7 3.8 3.4 3.0 2.3]
+	jump: cycle 1,
+		bre: [1.2 1.2]
+		hea: [1.2 1.2]
+		el1: [0.3 0.3]
+		hn1: [0.5 0.5]
+		el2: [0.5 0.5]
+		hn2: [0.7 0.7]
+		kn1: [3.8 3.8]
+		ft1: [3.8 3.8]
+		kn2: [3.7 3.7]
+		ft2: [2.7 2.7]
+	pos: v3(100,700,0)
+	vel: v3(0,0,0)
+	fixed: void
+	tick: (dt) ~>
+		@pts = pel: root:void, pos:v3(0,0,0)
+		cycle = if @fixed? then @climb else @jump
+		for member,ang of cycle now!
+			@pts[member] = 
+				pos:v3(cos(ang*tau/4),-sin(ang*tau/4),0)
+					.multiplyScalar(@anatomy[member].len*150)
+					.add(@pts[@anatomy[member].root].pos)
+				root:@pts[@anatomy[member].root]
+		if @fixed then
+			ds = @fixed.pos.clone().sub(@pts[@fixed.pivot].pos)
+			(->it.pos.add(ds)) `each` @pts
+			@vel = @pts.pel.pos.clone().sub(@pos).multiplyScalar(1.65)
+			@pos = @pts.pel.pos
+		else
+			@vel.add(v3(0,3*dt,0))
+			@pos.add(@vel)
+			(~>it.pos.add @pos) `each` @pts
+			arms 	|> filter (~> @pts.hn1.pos.distanceTo(it) < 22)
+					|> filter (~> it != @last_arm or it == arms[0])
+					|> each (~> @fixed = {pos:it, pivot:\hn1})
+		if @pos.y > 900 then
+			@pos = v3(100,700,0)
+			@vel = v3(0,0,0)
+	draw: (proc) ~>
+		proc.arc @pts.hea.pos.x, @pts.hea.pos.y, 9, 9, 0, tau
+		@pts	|> filter (->it.root?)
+				|> each (->proc.line it.pos.x, it.pos.y, it.root.pos.x, it.root.pos.y)
+monkey = Monkey!
+key.press \space ->
+	monkey.fixed = void
+last_tick = now!
+proc = processing 800, 800, ->
+	dt = now! - last_tick
+	last_tick := now!
 	@background 240 240 240
 	@strokeWeight 3
-	for a in vectors
-		#@arc a.pos.x, a.pos.y, 12, 12, 0, tau
-		for bound in a.bounds
-			b = bound.vector
-			if (a.pos? && b.pos?) then
-				@line a.pos.x, a.pos.y, b.pos.x, b.pos.y
+	monkey.tick dt
+	monkey.draw @
+	(~>@rect it.x, it.y, 5, 5) `each` arms
 proc.css \border "1px solid black"
 $("body").append proc
-
-person!
-tick!
-#lobo!
-
-
-# engine física: box2d.js, no idea how to use that shit
-# a lib carrega de boa. pra ver o conteudo dela: \/
-
